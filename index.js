4a92acf1ea816238b8bc28774e554352971c72b4
ImrangirHossain <imrangir.920@gmail.com>
2021-10-31 01:43:29 +0600

    Just a test commit to push new branch to heroku

--- index.js
+++ index.js
@@ -6,28 +6,37 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fniyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 async function run() {
     try {
         await client.connect();
+        console.log('hitting the post')
         const database = client.db('delivery');
         const servicesCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
 
         // get api
         app.get('/services', async(req, res)=>{
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services)
+         })
+        app.get('/orders', async(req, res)=>{
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders)
          })
 
         //  GET SINGLE SERVICES 
@@ -39,22 +48,39 @@ async function run() {
             res.send(service);
         })
        
+        //  GET SINGLE ORDER 
+        app.get('/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            console.log('getting single service', id)
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+       
         // POST API
         app.post('/services', async (req, res) => {
             const service = req.body;
-            console.log('hit the post api', service);
-
             const result = await servicesCollection.insertOne(service);
             console.log(result);
             res.json(result)
         });
+        
+         // Add Orders API 
+         app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
 
         // DELETE API
 
-        app.delete('/services/:id', async(req, res)=>{
+        app.delete('/orders/:id', async(req, res)=>{
             const id = req.params.id;
+            console.log(id)
             const query = { _id: ObjectId(id)};
-            const result = await servicesCollection.deleteOne(query);
+            const result = await orderCollection.deleteOne(query);
+            console.log('deleting user with id ', result);
+            res.json(result);
         })
         
 
@@ -65,10 +91,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get('/', (req,res)=>{
+    res.send('my  server are running')
+})
 
-app.get('/', (req, res)=>{
-    res.send();
-});
-app.listen(port, ()=>{
-    console.log('Node are running', port);
-});
+app.listen(port, () => {
+    console.log('Running  Server on port', port);
+})
